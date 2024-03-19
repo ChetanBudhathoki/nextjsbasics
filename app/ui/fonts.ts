b6110864617cd2870d4f4f894fcb22997fb105f0
ChetanBudhathoki <chetanbudhathoki1028@gmail.com>
2024-03-19 13:26:34 +0545

    lusitana removed

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import {Inter} from 'next/font/google';
 export const inter=Inter({subsets:['latin']});
 
 import { Lusitana } from 'next/font/google';
-export const lusty=Lusitana({subsets:['latin'],weight:"700"});
+// export const lusty=Lusitana({subsets:['latin'],weight:"700"});
