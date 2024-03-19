e18327487701ede40f861bbe4b8289019590ba44
ChetanBudhathoki <chetanbudhathoki1028@gmail.com>
2024-03-19 13:36:05 +0545

    changed

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import {Inter} from 'next/font/google';
 export const inter=Inter({subsets:['latin']});
 
-import { Lusitana } from 'next/font/google';
+// import { Lusitana } from 'next/font/google';
 // export const lusty=Lusitana({subsets:['latin'],weight:"700"});
